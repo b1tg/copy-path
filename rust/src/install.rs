4937ad02899b120f82f29ae8faebfa44c93ae651
b1 <b1tg@users.noreply.github.com>
2021-09-18 16:38:06 +0800

    Add backgroud menu entry: pop_cmd

--- rust/src/install.rs
+++ rust/src/install.rs
@@ -8,19 +8,49 @@ fn main() {
     let copy_path_raw = release_dir.join("copy_path_raw.exe");
     let copy_path_escape = release_dir.join("copy_path_escape.exe");
     let copy_md5 = release_dir.join("copy_md5.exe");
-    install("copy_path_raw", &copy_path_raw.to_string_lossy());
-    install("copy_path_escape", &copy_path_escape.to_string_lossy());
-    install("copy_md5", &copy_md5.to_string_lossy());
+    let pop_cmd = release_dir.join("pop_cmd.exe");
+    install(
+        "copy_path_raw",
+        &copy_path_raw.to_string_lossy(),
+        MenuTrig::File,
+    );
+    install(
+        "copy_path_escape",
+        &copy_path_escape.to_string_lossy(),
+        MenuTrig::File,
+    );
+    install("copy_md5", &copy_md5.to_string_lossy(), MenuTrig::File);
+    install("pop_cmd", &pop_cmd.to_string_lossy(), MenuTrig::Background);
+}
+#[derive(Eq, PartialEq)]
+enum MenuTrig {
+    ALL,
+    File,
+    Background,
 }
 
-fn install(menu: &str, exe: &str) {
-    let status = process::Command::new("cmd.exe")
-        .raw_arg("/c")
-        .raw_arg(format!(
-            r#"reg add HKEY_CLASSES_ROOT\*\shell\{}\command /ve /t REG_SZ /d "{} %1" /f"#,
-            menu, exe
-        ))
-        .status()
-        .unwrap();
-    println!("status: {:?}", status);
+/// ref: https://thegeekpage.com/add-any-program-to-right-click-context-menu/
+fn install(menu: &str, exe: &str, trig: MenuTrig) {
+    if trig == MenuTrig::File || trig == MenuTrig::ALL {
+        let status = process::Command::new("cmd.exe")
+            .raw_arg("/c")
+            .raw_arg(format!(
+                r#"reg add HKEY_CLASSES_ROOT\*\shell\{}\command /ve /t REG_SZ /d "{} %1" /f"#,
+                menu, exe
+            ))
+            .status()
+            .unwrap();
+        println!("status: {:?}", status);
+    }
+    if trig == MenuTrig::Background || trig == MenuTrig::ALL {
+        let status = process::Command::new("cmd.exe")
+                .raw_arg("/c")
+                .raw_arg(format!(
+                    r#"reg add HKEY_CLASSES_ROOT\Directory\Background\shell\{}\command /ve /t REG_SZ /d "{}" /f"#,
+                    menu, exe
+                ))
+                .status()
+                .unwrap();
+        println!("status: {:?}", status);
+    }
 }
